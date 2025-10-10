fa9c1a36b1a18a456b62b6ac610a4cd0e19d0cd7
FrantzFrankenStein <fmw13@case.edu>
2025-10-10 11:35:18 -0400

    protected auth merge conflicts

--- eventcompass/src/app/auth/page.tsx
+++ eventcompass/src/app/auth/page.tsx
@@ -1,21 +1,5 @@
 "use client";
 import React, { useState } from "react";
-<<<<<<< HEAD
-import styles from "../../css/LoginPage.module.css";
-
-export default function LoginPage() {
-  const [username, setUsername] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (username === "admin" && password === "pass") {
-      // alert("Login successful!");
-      window.location.href = "/";
-    } else {
-      alert("Invalid username or password.");
-=======
 import "../../css/globals.css";
 import styles from "../../css/LoginPage.module.css";
 import { supabase } from "../../lib/supabase/client";
@@ -38,7 +22,6 @@ export default function LoginPage() {
     } else {
       const data = await res.json();
       alert(data.error || "Login failed");
->>>>>>> bab3af6 (protected auth)
     }
   };
 
@@ -46,27 +29,6 @@ export default function LoginPage() {
     <div className={styles.container}>
       <div className={styles.card}>
         <h2 className={styles.title}>Login</h2>
-<<<<<<< HEAD
-        <form onSubmit={handleSubmit} className={styles.form}>
-          <input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            className={styles.input}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className={styles.input}
-            required
-          />
-          <button type="submit" className={styles.button}>
-            Login
-=======
         <form onSubmit={handleLogin} className={styles.form}>
           <input
             className={styles.input}
@@ -82,7 +44,6 @@ export default function LoginPage() {
           />
           <button className={styles.button} type="submit">
             Sign In
->>>>>>> bab3af6 (protected auth)
           </button>
         </form>
       </div>
